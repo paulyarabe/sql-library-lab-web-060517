74123c8b7928d048227ccf7f750cc07fdc250d39
github username <github email address>
2017-06-14 15:14:30 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT name, motto FROM characters WHERE length(motto) = (SELECT max(length(motto)) FROM characters);"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM series JOIN authors ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title FROM characters JOIN series ON characters.series_id = series.id WHERE characters.species = 'human' ORDER BY species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id GROUP BY character_id ORDER BY COUNT(book_id) DESC, characters.name ASC;"
 end
